7e13bafba7841b97aad087c97e1cba00dd663255
Ryan Lau <47727459+rynlu@users.noreply.github.com>
2020-12-20 19:17:37 -0500

    Upload files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+def parceladd(token, description, trknumber, courier):
+
+    headers = {"cookie": "token={}; language=en".format(token), "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36", "content-type": "application/x-www-form-urlencoded"}
+
+    data = "name={}&number={}&courier={}&postcode=&lang=en".format(description, trknumber, courier)
+
+    r = requests.post("https://web.parcelapp.net/add-ajax.php", headers=headers, data=data)
+
+    return r.text
+
+print(parceladd("TOKEN", "DESCRIPTION", "TRKNUMBER", "CARRIER"))
